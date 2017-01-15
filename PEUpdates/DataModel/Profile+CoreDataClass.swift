07cdb520131e06be579f00140806fa10d313ada5
Alexander Snegursky <a.snegurskiy@mobidev.biz>
2017-01-15 23:24:55 +0100

    Add Profile NSManagedObject

---
+++ PEUpdates/DataModel/Profile+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Profile+CoreDataClass.swift
+//  PEUpdates
+//
+//  Created by Alexander Snegursky on 1/15/17.
+//  Copyright © 2017 Alexander Snegursky. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Profile)
+public class Profile: NSManagedObject {
+
+}
